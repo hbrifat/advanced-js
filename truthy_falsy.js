7b4ff887d1e630dedf3d06ac53beb8d840878f63
hb rifat <hbrifat@gmail.com>
2020-08-07 11:03:09 +0600

    a file added

---
+++ truthy_falsy.js
@@ -0,0 +1,13 @@
+//truthy
+// "0", " ", {}, [], true
+
+//falsy
+// "", 0, false, undefined, null, NaN
+
+let name = "0";
+if(name) {
+  console.log("Condition is true");
+}
+else {
+  console.log("Condition is false");
+}
