4f13c459e34b955b0f94afe8afab1d4aa32d1d73
hb rifat <hbrifat@gmail.com>
2020-08-07 20:30:42 +0600

    anothe rfile added

---
+++ double_vs_triple_equal.js
@@ -0,0 +1,17 @@
+const value1 = 2;
+const value2 = "2";
+if(value1 == value2) {
+  console.log("Condition is true!");
+}
+else {
+  console.log("Condition is false!");
+}
+
+const value3 = 2;
+const value4 = "2";
+if(value3 === value4) {
+  console.log("Condition is true!");
+}
+else {
+  console.log("Condition is false!");
+}
