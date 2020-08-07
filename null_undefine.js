1feb39f16e9863a9a3e92dc57752f2b81241ef47
hb rifat <hbrifat@gmail.com>
2020-08-07 11:24:57 +0600

    undefined

---
+++ null_undefine.js
@@ -0,0 +1,17 @@
+      //undefined
+//1. let name;
+//2. function add(num1, num2) {
+//  num1 + num2;
+// (no return)
+//}
+// const result = add(13,82);
+//3. function add(num1, num2) {
+//  console.log(num1, num2); 
+//}
+//const result = add(13);
+//4. const omuk = {name: omuk, phone: 013456};
+//console.log(omuk.address);
+//5. let name = undefined;
+//console.log(name);
+
+console.log(name);
